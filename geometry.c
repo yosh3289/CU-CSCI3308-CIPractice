16cf8143e0b559acf5330a226dfcb2093e70cc8a
Yongbo Shu <yosh3289@colorado.edu>
2017-11-28 19:58:44 -0700

    final

--- geometry.c
+++ geometry.c
@@ -89,4 +89,3 @@ double coord_2d_area_triangle(const coord_2d_t* a, const coord_2d_t* b, const co
     /* Maths */
     return abs((a->x * (b->y - c->y) + b->x * (c->y - a->y) + c->x * (a->y - b->y))/2);
 }
-
